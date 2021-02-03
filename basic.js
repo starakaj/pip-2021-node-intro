54de0ad5746739bb239675ef8a9fb63852dfc178
Sam Tarakajian <sam@cycling74.com>
2021-02-03 11:42:54 -0500

    added a launch config

--- basic.js
+++ basic.js
@@ -1,5 +1,10 @@
 const os = require("os");
+const chance = require("chance");
+
+const mychange = new chance();
 
 console.log("Hi");
 
 console.log("It looks like you're running a " + os.platform() + " machine");
+
+console.log("Your new fake name is " + mychange.name);
